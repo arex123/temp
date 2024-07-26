2e5f4ec5d0c54aed98cd5f0664358a682e1ee5e6
arex123 <ad47kumar@gmail.com>
2024-07-26 20:01:45 +0530

    creating elements & dom relations

---
+++ Creating Elements & DOM Relations.js
@@ -0,0 +1,13 @@
+// Write your code below:
+let nh = document.createElement('h3')
+nh.appendChild(document.createTextNode('Buy high quality organic fruits online'))
+nh.style.fontStyle='italic'
+document.querySelector('div').appendChild(nh)
+
+let tf = document.createElement('p')
+
+tf.appendChild(document.createTextNode('Total fruits: 4'))
+tf.id = 'fruits-total'
+let sd = document.querySelectorAll('div')[1]
+let fruits =document.querySelector('.fruits')
+sd.insertBefore(tf,fruits)
